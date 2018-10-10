edc6ba148810620d0194fc0143327bf53c992670
Bharani <bharanidharan.nb@gmail.com>
2018-10-11 05:21:40 +0530

    Update browser target

--- packages/students/babel.config.js
+++ packages/students/babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        targets: '> 1%, last 2 versions, Firefox ESR',
+      },
+    ],
     '@babel/preset-react',
   ],
   ignore: ['node_modules', 'build'],
