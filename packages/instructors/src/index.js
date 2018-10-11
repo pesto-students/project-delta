8dec67c7c3e8efbcdc846e822905771528656c36
Bharani <bharanidharan.nb@gmail.com>
2018-10-12 03:54:36 +0530

    Common Package Boilerplate

--- packages/instructors/src/index.js
+++ packages/instructors/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { TestComponent } from 'delta-common';
+
 ReactDOM.render(
-  <h1>Welcome to Project Delta Instructors</h1>,
+  <h1>Welcome to Project Delta Instructors <TestComponent /></h1>,
   document.getElementById('root'),
 );
