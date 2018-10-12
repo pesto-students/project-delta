2d522802d8a0d32bc44d04fb508f68fa1b34b5cb
Bharani <bharanidharan.nb@gmail.com>
2018-10-13 02:14:57 +0530

    Add BlockButton

--- packages/shared-components/BlockButton.js
+++ packages/shared-components/BlockButton.js
@@ -1,4 +1,22 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { Colors } from './Theme';
 
-export const BlockButton = props => <Button fullWidth {...props} />;
+const styles = () => ({
+  button: {
+    backgroundColor: Colors.primary,
+    color: Colors.white,
+    textTransform: 'none',
+    '&:hover': {
+      backgroundColor: '#0069d9',
+    },
+  },
+  buttonFocused: {
+    backgroundColor: '#0069d9',
+  },
+});
+
+const BlockButton = ({ classes, ...props }) => <Button fullWidth variant="contained" className={classes.button} {...props} />;
+
+export default withStyles(styles)(BlockButton);
---
+++ packages/shared-components/Theme.js
@@ -0,0 +1,4 @@
+export const Colors = {
+  primary: '#007bff',
+  white: '#ffffff',
+};
