48e699a5b443b2f16f3125f3ad0dcc6e083516be
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-16 02:44:03 +0530

    Gave BoxComponent an interface to have its styles overridden

--- packages/shared-components/BoxComponent.js
+++ packages/shared-components/BoxComponent.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function BoxComponent(props) {
-  const { bgUrl, text } = props;
-
+function BoxComponent({
+  bgUrl,
+  text,
+  additionalBoxStyles,
+  additionalBoxImgStyles,
+  additionalBoxTextStyles,
+}) {
   const boxStyles = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
     alignItems: 'center',
+    ...additionalBoxStyles,
   };
 
   const boxImgStyles = {
     backgroundImage: `url('${bgUrl}')`,
     backgroundSize: 'cover',
+    ...additionalBoxImgStyles,
   };
 
   const boxTextStyles = {
     textAlign: 'center',
+    ...additionalBoxTextStyles,
   };
 
   return (
@@ -31,6 +38,15 @@ function BoxComponent(props) {
 BoxComponent.propTypes = {
   bgUrl: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  additionalBoxStyles: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  additionalBoxImgStyles: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  additionalBoxTextStyles: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+};
+
+BoxComponent.defaultProps = {
+  additionalBoxStyles: {},
+  additionalBoxImgStyles: {},
+  additionalBoxTextStyles: {},
 };
 
 export default BoxComponent;
