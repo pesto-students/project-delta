2a34105161f76cb84ed1b4ac2e5ef2d365b56b4f
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-16 04:51:02 +0530

    Revert "Added a complete CSS reset file to the project"
    This reverts commit 3a7d566e9d6c8fd19a913a4066b9217d3b64a0cb.

--- packages/students/src/index.js
+++ packages/students/src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/appComponent';
 
-import './reset.css';
-
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root'),
