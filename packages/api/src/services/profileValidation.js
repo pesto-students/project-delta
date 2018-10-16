d731c93049cc78c381134c276c9cecd0c3b50fd7
vipulrawat <vipulrawat.imi@live.in>
2018-10-17 03:48:35 +0530

    Backend of profile creation

---
+++ packages/api/src/services/profileValidation.js
@@ -0,0 +1,29 @@
+import isEmail from 'validator/lib/isEmail';
+
+const ERR_MSGS = require('../../constants/ERR_MSGS');
+
+function profileValidation(user) {
+  if (!Reflect.has(user, 'email')) {
+    return { passed: false, msg: ERR_MSGS.noEmail };
+  }
+  if (!isEmail(user.email)) {
+    return { passed: false, msg: ERR_MSGS.invalidEmail };
+  }
+  if (!Reflect.has(user, 'firstName') || user.firstName.length === 0) {
+    return { passed: false, msg: ERR_MSGS.noFirstName };
+  }
+  if (!Reflect.has(user, 'lastName') || user.lastName.length === 0) {
+    return { passed: false, msg: ERR_MSGS.noLastName };
+  }
+  if (!Reflect.has(user, 'role') || user.role.length === 0) {
+    return { passed: false, msg: ERR_MSGS.noRole };
+  }
+  if (user.role.toLowerCase() === 'student') {
+    if (!Reflect.has(user, 'batchId') || user.batchId.length < 24) {
+      return { passed: false, msg: ERR_MSGS.noBatchId };
+    }
+  }
+  return { passed: true, msg: 'passed' };
+}
+
+module.exports = profileValidation;
