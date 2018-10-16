44bbd59b226c483b3490795e45ef2fe3f7668b5e
Bharani <bharanidharan.nb@gmail.com>
2018-10-16 22:21:30 +0530

    Add LoginForm with Tests

---
+++ packages/students/src/components/Login/LoginForm.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import { BlockButton } from '../../../../shared-components/BlockButton';
+import { InputOutlined } from '../../../../shared-components/InputOutlined';
+import './style.css';
+
+class LoginForm extends Component {
+  state = {
+    email: '',
+  };
+
+  onEmailChange = (event) => {
+    this.setState({
+      email: event.currentTarget.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleLogin(this.state.email, this.clearEmail);
+  };
+
+  clearEmail = () => {
+    this.setState({
+      email: '',
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} data-testid="loginForm">
+        <InputOutlined
+          data-testid="email"
+          id="email"
+          type="email"
+          label="Email"
+          onChange={this.onEmailChange}
+          value={this.state.email}
+        />
+        <div className="login-button">
+          <BlockButton type="submit" data-testid="signin" isLoading={this.props.isLoggingIn}>
+            Signin
+          </BlockButton>
+        </div>
+      </form>
+    );
+  }
+}
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  isLoggingIn: PropTypes.bool.isRequired,
+};
+
+export { LoginForm };
