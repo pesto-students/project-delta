a9bc108a8e4f8b30946db40fa111ac3e684e5359
vipulrawat <vipulrawat.imi@live.in>
2018-10-17 22:55:09 +0530

    Backend of creating profile of student

--- packages/api/src/routes/profileRoutes.js
+++ packages/api/src/routes/profileRoutes.js
@@ -1,23 +1,28 @@
-import isEmail from 'validator/lib/isEmail';
-
 const profileRoutes = require('express').Router();
 const { User } = require('../db');
 const ERR_MSGS = require('../../constants/ERR_MSGS');
+const profileValidation = require('../services/profileValidation');
 
-profileRoutes.post('/saveData', (req, res) => {
-  if (!Reflect.has(req.body, 'email')) {
-    res.status(400).json({ error: ERR_MSGS.missingEmail });
-  } else if (typeof req.body.email !== 'string' || !isEmail(req.body.email)) {
-    res.status(400).json({ error: ERR_MSGS.invalidEmail });
+profileRoutes.post('/createUser', (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ error: ERR_MSGS.noProfileData });
   }
-  req.body.email = req.body.email.toLowerCase().trim();
-  const newUser = new User();
-  newUser.save((err, user) => { // eslint-disable-line
-    if (err) {
-      res.status(400).json({ error: 'Cannot save the user' });
-    }
-    res.status(200).json({ profileRoute: 'success', user: req.body.firstName });
-  });
+  const { body } = req;
+  if (!profileValidation(body).passed) {
+    return res.status(400).json({ error: profileValidation(body).msg });
+  }
+  body.batchId = body.batchId.toString();
+  const user = new User(body);
+  return user.save()
+    .then((doc) => {
+      if (!doc || doc.length === 0) {
+        return res.status(500).json({ error: ERR_MSGS.internalServerError });
+      }
+      return res.status(201).json({ user_created: 'Success', user_email: doc.email });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.errmsg });
+    });
 });
 
 module.exports = profileRoutes;
