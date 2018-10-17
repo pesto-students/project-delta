6fcbb34633a4d21e3c90d7d8872eee6147d84f5d
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-18 05:11:55 +0530

    HTTP service now sends token in requestsif present in localStorage

--- packages/students/src/services/http.js
+++ packages/students/src/services/http.js
@@ -1,11 +1,19 @@
 import { API_URL } from '../config';
 
 // adds json-content-type and origin headers if not present
-const processHeaders = headers => ({
-  'content-type': 'application/json; charset=utf-8',
-  origin: window.origin,
-  ...headers,
-});
+// if token in localstorage, add it as authorization header
+const processHeaders = (headers) => {
+  const defaultHeaders = {
+    'content-type': 'application/json; charset=utf-8',
+    origin: window.origin,
+  };
+
+  if (localStorage.getItem('token') !== null) {
+    defaultHeaders.authorization = localStorage.token;
+  }
+
+  return { ...defaultHeaders, ...headers };
+};
 
 // JSON.stringifies if appropriate, else passes it through
 const processBody = (body, headers) => (
