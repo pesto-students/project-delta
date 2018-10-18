e1784cac22b3ce9c4611f2ac759908a81d0ad8f2
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-19 05:16:14 +0530

    Now taking token via react-router match params instead of window.location in 'waiting for auth' page

--- packages/students/src/components/AppComponent.js
+++ packages/students/src/components/AppComponent.js
@@ -9,7 +9,7 @@ function AppComponent() {
   return (
     <React.Fragment>
       <Route path="/" exact component={LoginContainer} />
-      <Route path="/auth" component={AuthWaitingComponent} />
+      <Route path="/auth/:token" exact component={AuthWaitingComponent} />
       <Route path="/dashboard" exact component={DashboardContainer} />
     </React.Fragment>
   );
