eb7e9f8c26f76ede4c8e22f012278d376b57cb48
vipulrawat <vipulrawat.imi@live.in>
2018-10-18 22:23:46 +0530

    Moved isAuthenticated middleware to api/src/helper/auth for future use

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -9,26 +9,10 @@ const mailService = require('../services/mail');
 const models = require('../db');
 const ERR_MSGS = require('../../constants/ERR_MSGS');
 const TOKEN_TYPES = require('../../constants/TOKEN_TYPES');
-
+const isAuthenticated = require('../helper/auth/isAuthenticated');
 // Checks that the user who sent the request has a valid token
 // If authenticated, the decoded token is made available to next middleware at req.decoded
 // else, a 400 response is returned, and the next middleware is not called
-function isAuthenticated(req, res, next) {
-  const token = req.headers.Authorization || req.query.token
-    || req.cookies.token || req.body.token;
-  if (!token) {
-    res.status(400).json({ error: ERR_MSGS.invalidToken });
-  } else {
-    tokenService.decode(token)
-      .then((decoded) => {
-        req.decoded = decoded;
-        next();
-      })
-      .catch(e => res.status(400).json({
-        error: e.name === 'TokenExpiredError' ? ERR_MSGS.expiredToken : ERR_MSGS.invalidToken,
-      }));
-  }
-}
 
 routes.post('/generateToken', (req, res) => {
   if (!req.header('origin')) {
