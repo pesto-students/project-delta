6833d2e5b1b5e800503e7a183dd96d22bba1bf8c
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-18 16:14:28 +0530

    Bugfix: sign-in link sent in email was wrong

--- packages/api/src/services/mail.js
+++ packages/api/src/services/mail.js
@@ -5,12 +5,12 @@ const { SENDGRID_API_KEY } = require('../../config');
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendMail = (email, token, origin) => {
-  const link = `${origin}/verifyToken?token=${token}`;
+  const link = `${origin}/auth/${token}`;
   const msg = {
     to: email,
     from: 'deltapesto@gmail.com',
     subject: 'Welcome to Delta Pesto',
-    html: `Click <strong><a href="${link}">here</a></strong> to sign in`,
+    html: `Click <strong><a target="_blank" href="${link}">here</a></strong> to sign in`,
   };
   return Promise.resolve(sgMail.send(msg));
 };
