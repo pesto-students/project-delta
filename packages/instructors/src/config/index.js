fc6d894bd7004ba15403074c9616f67553711a38
Bharani <bharanidharan.nb@gmail.com>
2018-10-20 03:51:25 +0530

    Setup instructor config, service

---
+++ packages/instructors/src/config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  get API_URL() {
+    return (
+      // if we're running this app (students) locally,
+      //   we'll connect to the local api deployment
+      //   to avoid any possibility of messing with the prod DB
+      // else we're running this app in production on heroku,
+      //   so we'll connect to the production api deployment
+      /localhost/.test(window.location.hostname)
+        ? 'http://localhost:5000'
+        : 'https://deltapesto-api.herokuapp.com'
+    );
+  },
+};
---
+++ packages/instructors/src/services/http.js
@@ -0,0 +1,45 @@
+import { API_URL } from '../config';
+
+// adds json-content-type and origin headers if not present
+const addDefaultReqHeaders = (headers) => {
+  const defaultHeaders = {
+    'content-type': 'application/json; charset=utf-8',
+    origin: window.origin,
+  };
+
+  return { ...defaultHeaders, ...headers };
+};
+
+// JSON.stringifies if appropriate, else passes it through
+const processReqBody = (body, headers) => (
+  /^application\/json/.test(headers['content-type'])
+    && typeof body === 'object'
+    ? JSON.stringify(body)
+    : body
+);
+
+const extractResponseBody = (res) => {
+  if (/application\/json/.test(res.headers.get('content-type'))) {
+    return res.json();
+  }
+  return res.text();
+};
+
+export const HTTP = {
+  POST: (urlPath, body, headers = {}, shouldExtractResponseBody = true) => {
+    const finalHeaders = addDefaultReqHeaders(headers);
+
+    return fetch(`${API_URL}${urlPath}`, {
+      method: 'POST',
+      headers: finalHeaders,
+      body: processReqBody(body, finalHeaders),
+      credentials: 'include',
+    }).then((response) => {
+      if (shouldExtractResponseBody) {
+        return extractResponseBody(response);
+      }
+
+      return response;
+    });
+  },
+};
