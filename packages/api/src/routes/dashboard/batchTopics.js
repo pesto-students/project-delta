f1f5b7968733858e6f5f664fc61a83c3c26da54d
vipulrawat <vipulrawat.imi@live.in>
2018-10-20 20:02:40 +0530

    protected routes and add GET method /gettopics/:day

--- packages/api/src/routes/dashboard/batchTopics.js
+++ packages/api/src/routes/dashboard/batchTopics.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 import { asyncHandler } from '../../services/asyncHandler';
 import { getBatchTopics, insertBatchTopics } from '../../db/collections/batchTopics';
+import { isAuthenticated } from '../../helper/auth/isAuthenticated';
 
 const batchTopicRoutes = Router();
 
-batchTopicRoutes.get('/', asyncHandler(async (req, res, next) => {
+batchTopicRoutes.get('/', isAuthenticated, asyncHandler(async (req, res, next) => {
   const topicsList = await getBatchTopics();
   res.json({ topicsList });
   next();
 }));
-
-batchTopicRoutes.post('/', asyncHandler(async (req, res, next) => {
+batchTopicRoutes.get('/:day', isAuthenticated, asyncHandler(async (req, res, next) => {
+  const topicsList = await getBatchTopics(req.params.day);
+  res.json({ topicsList });
+  next();
+}));
+batchTopicRoutes.post('/', isAuthenticated, asyncHandler(async (req, res, next) => {
   const result = await insertBatchTopics();
   res.json({ result });
   next();
