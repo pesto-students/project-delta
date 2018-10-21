c0c6693ee90630f9efec99674d261d11e6d3ce5c
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-21 14:34:26 +0530

    Added a user service to abstract requests involving user data
    Methods are mocked; this will be changed when backend is ready for integration

---
+++ packages/students/src/services/user.js
@@ -0,0 +1,20 @@
+// import { HTTP as httpService } from './http';
+
+export function getUserProfile(id = 'me') { // eslint-disable-line no-unused-vars
+  // return httpService.GET(`/users/${id}`);
+  return Promise.resolve({
+    email: 'anirudh.nimmagadda@gmail.com',
+    firstName: 'Anirudh',
+    lastName: 'Nimmagadda',
+    role: 'student',
+    profilePicUrl: 'https://lh3.googleusercontent.com/-b2fwL216_m0/AAAAAAAAAAI/AAAAAAAAAAA/ZJocaz5rHUk/photo.jpg',
+    batchId: '111111111111222222222222',
+    batchCity: 'New Delhi',
+    batchNumber: '2',
+  });
+}
+
+export function updateUserProfile(newData, id = 'me') { // eslint-disable-line no-unused-vars
+  // return httpService.POST(`/users/${id}`, newData);
+  return Promise.resolve(newData);
+}
