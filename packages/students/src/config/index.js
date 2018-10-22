169c8b6c83690df324d0291dbcacbce54c29d4e0
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-23 03:24:14 +0530

    Added skeleton edit-profile component

--- packages/students/src/config/index.js
+++ packages/students/src/config/index.js
@@ -1,3 +1,14 @@
 import sharedConfig from '../../../shared-utils/config';
 
-export const { API_URL, DATE_FORMAT } = sharedConfig;
+const FIREBASE = {
+  apiKey: 'AIzaSyAKkYpQxqGCEfsF_XRsmS1IgOVAo5_nOKc',
+  authDomain: 'project-delta-220322.firebaseapp.com',
+  databaseURL: 'https://project-delta-220322.firebaseio.com',
+  projectId: 'project-delta-220322',
+  storageBucket: 'project-delta-220322.appspot.com',
+  messagingSenderId: '43481274221',
+};
+
+const { API_URL, DATE_FORMAT } = sharedConfig;
+
+export { API_URL, DATE_FORMAT, FIREBASE };
