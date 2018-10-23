101caf368a4c139df36d9d7858d46441b6290f38
Bharani <bharanidharan.nb@gmail.com>
2018-10-24 03:40:57 +0530

    Refractor services, config > shared-utils

--- packages/students/src/config/index.js
+++ packages/students/src/config/index.js
@@ -1,14 +1,3 @@
-module.exports = {
-  get API_URL() {
-    return (
-      // if we're running this app (students) locally,
-      //   we'll connect to the local api deployment
-      //   to avoid any possibility of messing with the prod DB
-      // else we're running this app in production on heroku,
-      //   so we'll connect to the production api deployment
-      /localhost/.test(window.location.hostname)
-        ? 'http://localhost:5000'
-        : 'https://deltapesto-api.herokuapp.com'
-    );
-  },
-};
+import sharedConfig from '../../../shared-utils/config';
+
+export const { API_URL, DATE_FORMAT } = sharedConfig;
