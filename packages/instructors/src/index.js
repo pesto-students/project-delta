59b43c0adad1e8544375939161fe4eda6ac98365
Bharani <bharanidharan.nb@gmail.com>
2018-10-23 22:27:09 +0530

    Add React Router

--- packages/instructors/src/index.js
+++ packages/instructors/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import JssProvider from 'react-jss/lib/JssProvider';
 
-import { NewBatchContainer } from './components/Batch/NewBatchContainer';
+import App from './components/App';
+
+import './main.css';
 
 ReactDOM.render(
-  <NewBatchContainer />,
+  <BrowserRouter><JssProvider><App /></JssProvider></BrowserRouter>,
   document.getElementById('root'),
 );
