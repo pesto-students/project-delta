921b0c26b0a55ac1d96ab2e1813f3f5b24401737
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-29 22:11:41 +0530

    Students/profile now has fields for sex and dob (#118)
    * Added sex and dob fields to edit profile page
    
    * Mock user service now returns sex and dob of user
    
    * Resized male/female default profile pics
    
    * Added sex and dob fields to profile view component + some styling changes
    
    * Minor bugfix + refactor

--- packages/students/src/components/ProfilePage/StudentProfileView.js
+++ packages/students/src/components/ProfilePage/StudentProfileView.js
@@ -5,12 +5,19 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 import { LoginHeader, LoginFooter } from '../../../../shared-components/LoginComponents';
-import { DEFAULT_PROFILE_PIC_URL as defaultProfilePicUrl } from '../../config';
-import { userProfilePropType } from './userProfilePropType';
+import { getAppropriateDefaultProfilePic, userProfilePropType } from './shared';
 
 import './StudentProfileView.css';
 
 export function StudentProfileViewComponent({ handleEditBtnClick, userData }) {
+  const profilePicUrl = userData.profilePicUrl
+    || getAppropriateDefaultProfilePic(userData.sex);
+
+  let sex;
+  if (userData.sex === 'm') sex = 'Male';
+  else if (userData.sex === 'f') sex = 'Female';
+  else sex = 'A Mystery';
+
   return (
     <Grid container justify="center">
       <Grid item xs={12} md={8} lg={7}><LoginHeader /></Grid>
@@ -32,37 +39,52 @@ export function StudentProfileViewComponent({ handleEditBtnClick, userData }) {
       </Grid>
 
       <Grid item container className="profile" xs={12} md={8} lg={7} justify="center" spacing={40}>
-        <Grid item xs={6} md={3} className="profile-pic">
+        <Grid item xs={10} md={5} className="profile-pic">
           <img
-            src={userData.profilePicUrl || defaultProfilePicUrl}
+            src={profilePicUrl}
             alt={`${userData.firstName} ${userData.lastName}`}
             title="Profile Picture"
           />
         </Grid>
 
-        <Grid item container xs={10} md={6} className="profile-details">
+        <Grid item container xs={10} md={7} className="profile-details">
           <Grid item container className="name" spacing={8}>
-            <Grid item xs={4} md={2}>Name:</Grid>
+            <Grid item xs={4}>Name:</Grid>
             <Grid item className="first-name">{userData.firstName}</Grid>
             <Grid item className="last-name">{userData.lastName}</Grid>
           </Grid>
 
           <Grid item container className="email">
-            <Grid item xs={4} md={2}>Email:</Grid>
+            <Grid item xs={4}>Email:</Grid>
             <Grid item className="email-address">{userData.email}</Grid>
           </Grid>
 
           <Grid item container className="batch" spacing={8}>
-            <Grid item xs={4} md={2}>Batch:</Grid>
+            <Grid item xs={4}>Batch:</Grid>
             <Grid item className="batch-city">{userData.batchCity}</Grid>
             <Grid item className="batch-number">
               {userData.batchNumber === undefined ? '' : `#${userData.batchNumber}`}
             </Grid>
           </Grid>
 
-          <Grid item>
-            <Button variant="contained" color="primary" onClick={handleEditBtnClick}>
-              Edit Profile
+          <Grid item container>
+            <Grid item xs={4}>Date of Birth:</Grid>
+            <Grid item className="date-of-birth">{userData.dob || 'Unknown'}</Grid>
+          </Grid>
+
+          <Grid item container>
+            <Grid item xs={4}>Sex:</Grid>
+            <Grid item className="sex">{sex}</Grid>
+          </Grid>
+
+          <Grid item xs={5}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleEditBtnClick}
+              style={{ width: '100%' }}
+            >
+              Edit
             </Button>
           </Grid>
         </Grid>
