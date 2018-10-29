376099e2b40486ec460b86c27a57501695f1edc5
vipulrawat <vipulrawat.imi@live.in>
2018-10-30 03:33:08 +0530

    Modified the POST submission method of user's topics

--- packages/api/src/db/collections/userTopics.js
+++ packages/api/src/db/collections/userTopics.js
@@ -1,15 +1,7 @@
 import { UserTopic } from '../index';
 
-const insertUserTopic = async (topic) => {
-  const topicInfo = {
-    userId: topic.userId,
-    userFirstName: topic.userFirstName,
-    batchTopicId: topic.batchTopicId,
-    batchTopicName: topic.batchTopicName,
-    rating: topic.rating,
-  };
-  const newUserTopic = new UserTopic(topicInfo);
-  const result = await newUserTopic.save();
+const insertUserTopic = async (topics) => {
+  const result = await UserTopic.insertMany(topics);
   return result;
 };
 
