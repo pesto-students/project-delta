2a441a926aa4f59b6bbdffd605bf243a57257e0d
vipulrawat <vipulrawat.imi@live.in>
2018-10-29 21:55:26 +0530

    Added GET route for the list of user's review on topics

--- packages/api/src/routes/userTopics.js
+++ packages/api/src/routes/userTopics.js
@@ -2,16 +2,24 @@ import { Router } from 'express';
 import { asyncHandler } from '../services/asyncHandler';
 import {
   insertUserTopic,
+  getUserTopics,
 } from '../db/collections/userTopics';
 import { isAuthenticated } from '../helper/auth/isAuthenticated';
 import { extractUser } from '../helper/user';
 
 const userTopic = Router();
 
-userTopic.post('/create', isAuthenticated, extractUser, asyncHandler(async (req, res) => {;
-  const data = { userId: req.user._id,
+userTopic.get('/list', isAuthenticated, extractUser, asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const userTopicList = await getUserTopics(userId);
+  return res.json({ userTopicList });
+}));
+
+userTopic.post('/create', isAuthenticated, extractUser, asyncHandler(async (req, res) => {
+  const data = {
+    userId: req.user._id,
     userFirstName: req.user.firstName,
-    ...req.body,       
+    ...req.body,
   };
   const newTopic = await insertUserTopic(data);
   return res.json({ newTopic });
