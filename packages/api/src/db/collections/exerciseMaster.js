8fceb13d2df97a1829685aa23aaade0bad474549
Bharani <bharanidharan.nb@gmail.com>
2018-10-31 03:45:21 +0530

    ExerciseMaster: Update schema

--- packages/api/src/db/collections/exerciseMaster.js
+++ packages/api/src/db/collections/exerciseMaster.js
@@ -4,15 +4,29 @@ const getAllExerciseMaster = async () => {
   const projection = {
     name: 1,
     topicId: 1,
+    topicName: 1,
+    day: 1,
   };
-  const exerciseList = await ExerciseMaster.find({}, projection);
+  const query = {
+    archive: false,
+  };
+  const exerciseList = await ExerciseMaster.find(query, projection);
   return exerciseList;
 };
 
 const insertNewExercise = async (excersiseDetail) => {
-  const newExercise = new ExerciseMaster(excersiseDetail);
+  const newExercise = new ExerciseMaster({
+    ...excersiseDetail,
+    archive: false,
+  });
   const result = await newExercise.save();
-  return result.id;
+  return {
+    _id: result._id,
+    name: result.name,
+    topicId: result.topicId,
+    topicName: result.topicName,
+    day: result.day,
+  };
 };
 // update on exercise will only take new exercise name and exercise ID
 const updateExerciseMaster = async (exerciseId, newData) => {
@@ -28,10 +42,14 @@ const updateExerciseMaster = async (exerciseId, newData) => {
 };
 
 const deleteExerciseMaster = async (exerciseId) => {
+  const updatedInfo = {
+    archive: true,
+  };
   const query = {
     _id: exerciseId,
   };
-  const result = await ExerciseMaster.deleteOne(query);
+  const opts = { runValidators: true };
+  const result = await ExerciseMaster.update(query, { $set: updatedInfo }, opts);
   return result;
 };
 
