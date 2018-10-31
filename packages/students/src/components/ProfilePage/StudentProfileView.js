01c22020a60dcbd05d9e933a6c2a514c802981ea
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-31 15:46:02 +0530

    View profile screen is now fully responsive

--- packages/students/src/components/ProfilePage/StudentProfileView.js
+++ packages/students/src/components/ProfilePage/StudentProfileView.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
+import { BlockButton } from '../../../../shared-components/BlockButton';
 import { LoginHeader, LoginFooter } from '../../../../shared-components/LoginComponents';
 import { getAppropriateDefaultProfilePic, userProfilePropType } from './shared';
-
-import './StudentProfileView.css';
 import { NavigateToDashboardComponent } from './NavigateToDashboard';
 
 export function StudentProfileViewComponent({ handleEditBtnClick, userData }) {
@@ -16,69 +15,55 @@ export function StudentProfileViewComponent({ handleEditBtnClick, userData }) {
   let sex;
   if (userData.sex === 'm') sex = 'Male';
   else if (userData.sex === 'f') sex = 'Female';
-  else sex = 'A Mystery';
+  else sex = 'A real mystery';
 
   return (
     <Grid container justify="center">
-      <Grid item xs={12} md={8} lg={7}><LoginHeader /></Grid>
+      <Grid item xs={12} md={8}><LoginHeader /></Grid>
 
       <Grid item xs={12} md={8} className="navigation">
         <NavigateToDashboardComponent />
       </Grid>
 
-      <Grid item container className="profile" xs={12} md={8} lg={7} justify="center" spacing={40}>
-        <Grid item xs={10} md={5} className="profile-pic">
-          <img
-            src={profilePicUrl}
-            alt={`${userData.firstName} ${userData.lastName}`}
-            title="Profile Picture"
+      <Grid className="profile" container justify="center" item xs={12} md={8} spacing={40}>
+        <Grid
+          container
+          justify="center"
+          item
+          xs={12}
+          sm={5}
+          className="profile-pic"
+        >
+          <div
+            className="profile-pic-holder"
+            style={{
+              backgroundImage: `url('${profilePicUrl}')`,
+              backgroundSize: 'cover',
+              height: '250px',
+              width: '250px',
+            }}
           />
         </Grid>
 
-        <Grid item container xs={10} md={7} className="profile-details">
-          <Grid item container className="name" spacing={8}>
-            <Grid item xs={4}>Name:</Grid>
-            <Grid item className="first-name">{userData.firstName}</Grid>
-            <Grid item className="last-name">{userData.lastName}</Grid>
-          </Grid>
+        <Grid
+          direction="column"
+          alignItems="center"
+          item
+          xs={12}
+          sm={7}
+          className="profile-details"
+        >
+          <ProfileDetailComponent name="Name" value={`${userData.firstName} ${userData.lastName}`} />
+          <ProfileDetailComponent name="Email" value={userData.email} />
+          <ProfileDetailComponent name="Batch" value={`${userData.batchCity} #${userData.batchNumber}`} />
+          <ProfileDetailComponent name="DOB" value={userData.dob || 'Unknown (very old)'} />
+          <ProfileDetailComponent name="Sex" value={sex} />
 
-          <Grid item container className="email">
-            <Grid item xs={4}>Email:</Grid>
-            <Grid item className="email-address">{userData.email}</Grid>
-          </Grid>
-
-          <Grid item container className="batch" spacing={8}>
-            <Grid item xs={4}>Batch:</Grid>
-            <Grid item className="batch-city">{userData.batchCity}</Grid>
-            <Grid item className="batch-number">
-              {userData.batchNumber === undefined ? '' : `#${userData.batchNumber}`}
-            </Grid>
-          </Grid>
-
-          <Grid item container>
-            <Grid item xs={4}>Date of Birth:</Grid>
-            <Grid item className="date-of-birth">{userData.dob || 'Unknown'}</Grid>
-          </Grid>
-
-          <Grid item container>
-            <Grid item xs={4}>Sex:</Grid>
-            <Grid item className="sex">{sex}</Grid>
-          </Grid>
-
-          <Grid item xs={5}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleEditBtnClick}
-              style={{ width: '100%' }}
-            >
-              Edit
-            </Button>
-          </Grid>
+          <BlockButton onClick={handleEditBtnClick}>EDIT</BlockButton>
         </Grid>
       </Grid>
 
-      <Grid item xs={10} md={8} lg={7}><LoginFooter /></Grid>
+      <Grid item xs={12} md={8} style={{ paddingTop: '30px' }}><LoginFooter /></Grid>
     </Grid>
   );
 }
@@ -90,4 +75,24 @@ StudentProfileViewComponent.propTypes = {
 
 StudentProfileViewComponent.defaultProps = {
   userData: {},
+};
+
+function ProfileDetailComponent({ name, value }) {
+  return (
+    <Paper style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      padding: '20px',
+      marginBottom: '20px',
+    }}
+    >
+      <span>{`${name}:`}</span>
+      <span>{value}</span>
+    </Paper>
+  );
+}
+
+ProfileDetailComponent.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
 };
