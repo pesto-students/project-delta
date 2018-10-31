7b5412083249a8b7b98f4d4de4edcc3bde505b87
vipulrawat <vipulrawat.imi@live.in>
2018-11-01 02:25:53 +0530

    Changed Profile Routes

--- packages/api/src/services/profileValidation.js
+++ packages/api/src/services/profileValidation.js
@@ -1,4 +1,5 @@
 import isEmail from 'validator/lib/isEmail';
+import Batch from '../db';
 
 const ERR_MSGS = require('../../constants/ERR_MSGS');
 
@@ -20,7 +21,10 @@ function profileValidation(user) {
   }
   if (user.role.toLowerCase() === 'student') {
     if (!Reflect.has(user, 'batchId') || user.batchId.length < 24) {
-      return { passed: false, msg: ERR_MSGS.noBatchId };
+      const batchIdInDB = Batch.findOne({ _id: user.batchId });
+      if (!batchIdInDB) {
+        return { passed: false, msg: ERR_MSGS.noBatchId };
+      }
     }
   }
   return { passed: true, msg: 'passed' };
