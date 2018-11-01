bf9a30da3d7b3e761972964b35fab09c551f8856
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-11-01 23:35:54 +0530

    Improved HeaderComponent (still doesn't have state)

--- packages/shared-components/Header.js
+++ packages/shared-components/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Divider from '@material-ui/core/Divider';
 
+import { ImageBoxComponent } from './ImageBox';
+
+import logo from './assets/logo.png';
+
 export function HeaderComponent() {
   return (
-    <header>
-      <h1 className="text-center">Project Delta</h1>
+    <React.Fragment>
+      <header style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+      >
+        <div style={{ display: 'flex', alignItems: 'baseline' }}>
+          <h1>Project</h1>
+          <ImageBoxComponent bgUrl={logo} style={{ borderRadius: '0px', marginLeft: '10px' }} />
+        </div>
+      </header>
       <Divider />
-    </header>
+    </React.Fragment>
   );
 }
