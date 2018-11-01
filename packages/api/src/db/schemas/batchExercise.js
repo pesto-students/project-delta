b99d865eccf11c1748a6cb32d3482998d907a7b0
Bharani <bharanidharan.nb@gmail.com>
2018-11-02 00:24:29 +0530

    Update batch exercise schema

--- packages/api/src/db/schemas/batchExercise.js
+++ packages/api/src/db/schemas/batchExercise.js
@@ -8,13 +8,37 @@ const batchExerciseSchema = new Schema({
     minlength: 1,
   },
 
+  day: {
+    type: Number,
+    required: true,
+    min: 1,
+    validator: Number.isInteger,
+  },
+
   batchTopicId: { // link to 'batch-topics' collection
     type: Schema.Types.ObjectId,
     required: true,
   },
 
+  batchTopicName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+
   batchId: { // link to 'batches' collection
     type: Schema.Types.ObjectId,
+    required: true,
+  },
+
+  masterId: { // link to 'master exercise' collection
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+
+  archive: { // Represent deleted documents
+    type: Boolean,
     required: true,
   },
 });
