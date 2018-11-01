63e9671efcf52ae2f5ed57d5ea5fb7403eff2328
Bharani <bharanidharan.nb@gmail.com>
2018-11-02 04:05:05 +0530

    Add delete route to batch exercise

--- packages/api/src/db/collections/__tests__/batchExercise.test.js
+++ packages/api/src/db/collections/__tests__/batchExercise.test.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { DB_URL } from '../../../../config';
 
-import { getAllExercise } from '../batchExercise';
+import { getAllExercise, deleteBatchExercise } from '../batchExercise';
 import { BatchExercise } from '../../index';
 
 describe('Mongo Queries: Batch Exercises', () => {
@@ -68,6 +68,34 @@ describe('Mongo Queries: Batch Exercises', () => {
       const batchTopicsList = await getAllExercise(topicId, day);
 
       expect(batchTopicsList.length).toBe(1);
+    });
+  });
+
+  describe('Delete Exercise Batch', () => {
+    const newExercise = {
+      batchId: mongoose.Types.ObjectId('111111111111111111111112'),
+      name: 'Newest Exercise 1',
+      batchTopicId: mongoose.Types.ObjectId('121212121212121212121215'),
+      batchTopicName: 'Javascript',
+      day: 2,
+      archive: false,
+      masterId: mongoose.Types.ObjectId('master_id_05'),
+    };
+
+    beforeEach(async () => {
+      const exerciseModel = new BatchExercise(newExercise);
+      const insertedDocument = await exerciseModel.save();
+      newExercise._id = insertedDocument._id;
+    });
+
+    afterEach(async () => {
+      await BatchExercise.deleteOne(newExercise);
+    });
+
+    it('should update document archive value', async () => {
+      await deleteBatchExercise(newExercise._id);
+      const exercise = await BatchExercise.findOne(newExercise._id);
+      expect(exercise.archive).toBe(true);
     });
   });
 });
--- packages/api/src/routes/batchExercise.js
+++ packages/api/src/routes/batchExercise.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { Types } from 'mongoose';
 
 import { missingInfo, noBatchId } from '../../constants/ERR_MSGS';
-import { getAllExercise } from '../db/collections/batchExercise';
+import { getAllExercise, deleteBatchExercise } from '../db/collections/batchExercise';
 import { isAuthenticated } from '../helper/auth/isAuthenticated';
 import { extractUser } from '../helper/user';
 import { asyncHandler } from '../services/asyncHandler';
@@ -20,6 +20,12 @@ batchExercise.get('/list', isAuthenticated, extractUser, asyncHandler(async (req
   }
   const exerciseList = await getAllExercise(batchId, day);
   return res.json({ exerciseList });
+}));
+
+batchExercise.delete('/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await deleteBatchExercise(id);
+  return res.json({ status: 'Success' });
 }));
 
 export { batchExercise };
